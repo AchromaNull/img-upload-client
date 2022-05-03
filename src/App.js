6346b848c3a7b1767d5c5b95570fcbb644f7e0ca
robinson4623@gmail.com <robinson4623@gmail.com>
2022-05-03 10:49:14 -0600

    Moved AWS upload function to Upload.js as a function component
    App.js was revised to point to the Upload.js component.
    
    App.js was reworked as a class component
    
    Co-author: rayrott https://github.com/raytrott
    Co-author: BlasAgosto https://github.com/BlasAgosto

--- src/App.js
+++ src/App.js
@@ -1,90 +1,93 @@
+/* eslint-disable brace-style */
+/* eslint-disable block-spacing */
+/* eslint-disable no-mixed-spaces-and-tabs */
 /* eslint-disable no-tabs */
-import React, { useState } from 'react'
-import axios from 'axios'
-import apiUrl from './apiConfig'
-// import { Route } from 'react-router-dom'
-// import { v4 as uuid } from 'uuid'
-import Form from 'react-bootstrap/Form'
-import Button from 'react-bootstrap/Button'
-
-// import AuthenticatedRoute from './components/AuthenticatedRoute/AuthenticatedRoute'
-// import AutoDismissAlert from './components/AutoDismissAlert/AutoDismissAlert'
-// import Header from './components/Header/Header'
-// import SignUp from './components/auth/SignUp'
-// import SignIn from './components/auth/SignIn'
-// import SignOut from './components/auth/SignOut'
-// import ChangePassword from './components/auth/ChangePassword'
-
-const FormData = require('form-data')
-
-function App () {
-  const [selected, setSelected] = useState(null)
-  const [upload, setUpload] = useState({})
-  const [loading, setLoading] = useState(false)
-
-  const handleChange = (event) => {
-    setSelected(event.target.files[0])
+import React, { Component, Fragment } from 'react'
+import { Route } from 'react-router-dom'
+import { v4 as uuid } from 'uuid'
+import AuthenticatedRoute from './components/AuthenticatedRoute/AuthenticatedRoute'
+import AutoDismissAlert from './components/AutoDismissAlert/AutoDismissAlert'
+import Header from './components/Header/Header'
+import SignUp from './components/auth/SignUp'
+import SignIn from './components/auth/SignIn'
+import SignOut from './components/auth/SignOut'
+import ChangePassword from './components/auth/ChangePassword'
+import Upload from './Upload'
+class App extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      user: null,
+      msgAlerts: []
+    }
   }
-  const handleSubmit = (event) => {
-    event.preventDefault()
-    setLoading(true)
 
-    const data = new FormData()
-    data.append('upload', selected)
-    axios({
-      url: apiUrl + '/uploads',
-      method: 'POST',
-      data
+  setUser = (user) => this.setState({ user })
+  clearUser = () => this.setState({ user: null })
+  deleteAlert = (id) => {this.setState((state) => {
+    return { msgAlerts: state.msgAlerts.filter((msg) => msg.id !== id) }
+  })
+  }
+
+  msgAlert = ({ heading, message, variant }) => {
+    const id = uuid()
+    this.setState((state) => {
+      return {
+        msgAlerts: [...state.msgAlerts, { heading, message, variant, id }]
+      }
     })
-      .then(res => setUpload(res.data.upload))
-      .then(() => setLoading(false))
-      .catch(console.error)
   }
-  // [selected, setSelected] = useState(null)
-  //  handleChange = (event) => {
-  //    console.log(event.target.files[0])
-  //    setSelected(event.target.files[0])
-  //  }
-  // componentDidMount () {
-  //   console.log('Kiss it')
-  // }
 
-  // setUser = (user) => this.setState({ user })
-
-  // clearUser = () => this.setState({ user: null })
-
-  // deleteAlert = (id) => {
-  //   this.setState((state) => {
-  //     return { msgAlerts: state.msgAlerts.filter((msg) => msg.id !== id) }
-  //   })
-  // }
-
-  // msgAlert = ({ heading, message, variant }) => {
-  //   const id = uuid()
-  //   this.setState((state) => {
-  //     return {
-  //       msgAlerts: [...state.msgAlerts, { heading, message, variant, id }]
-  //     }
-  //   })
-  // }
-
-  // const { msgAlerts, user } = this.state
-
-  return (
-
-    <div className="App">
-      {upload.url ? (<img className="display-image" alt={upload.url} src={upload.url} />) : ''}
-      {loading ? (<img className="display-loading" alt="loading gif" src="https://cutewallpaper.org/21/loading-gif-transparent-background/Download-Loading-Gif-Generator-Transparent-Background-PNG-.gif"/>) : ''}
-      <Form onSubmit={handleSubmit}>
-        <Form.Group controlId="formFile" className="mb-3" id="upload-file-input" >
-          <Form.Label>Upload Your Pic Yo</Form.Label>
-          <Form.Control type="file" onChange={handleChange} />
-          <Button variant="primary" type="submit" value="Submit" >Submit</Button>
-        </Form.Group>
-      </Form>
-    </div>
-
-  )
+  render () {
+    const { msgAlerts, user } = this.state
+    return (
+      <Fragment>
+        <Header user={user} />
+        {msgAlerts.map((msgAlert) => (
+          <AutoDismissAlert
+            key={msgAlert.id}
+            heading={msgAlert.heading}
+            variant={msgAlert.variant}
+            message={msgAlert.message}
+            id={msgAlert.id}
+            deleteAlert={this.deleteAlert}
+          />
+        ))}
+        <main className='container'>
+          <Route
+            path='/sign-up'
+            render={() => (
+              <SignUp msgAlert={this.msgAlert} setUser={this.setUser} />
+            )}
+          />
+          <Route
+            path='/sign-in'
+            render={() => (
+              <SignIn msgAlert={this.msgAlert} setUser={this.setUser} />
+            )}
+          />
+          <AuthenticatedRoute
+            user={user}
+            path='/sign-out'
+            render={() => (
+              <SignOut
+                msgAlert={this.msgAlert}
+                clearUser={this.clearUser}
+                user={user}
+              />
+            )}
+          />
+          <AuthenticatedRoute
+            user={user}
+            path='/change-password'
+            render={() => (
+              <ChangePassword msgAlert={this.msgAlert} user={user} />
+            )}
+          />
+          <Upload />
+        </main>
+      </Fragment>
+    )
+  }
 }
-
 export default App
---
+++ src/Upload.js
@@ -0,0 +1,50 @@
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
+import React, { useState } from 'react'
+import axios from 'axios'
+import apiUrl from './apiConfig'
+
+const FormData = require('form-data')
+
+export default function Upload () {
+  const [selected, setSelected] = useState(null)
+  const [upload, setUpload] = useState({})
+  const [loading, setLoading] = useState(false)
+
+  // const [user, setUser] = useState(user)
+  const handleChange = (event) => {
+    setSelected(event.target.files[0])
+  }
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    setLoading(true)
+    const data = new FormData()
+    data.append('upload', selected)
+    axios({
+      url: apiUrl + '/uploads',
+      method: 'POST',
+      data
+    })
+      .then(res => setUpload(res.data.upload))
+      .then(() => setLoading(false))
+      .catch(console.error)
+  }
+  return (
+    <div>
+      {upload.url ? (<img className='display-image' alt={upload.url} src={upload.url} />) : ('')}
+      {loading
+        ? (<img className='display-loading' alt='loading gif'
+          src='https://cutewallpaper.org/21/loading-gif-transparent-background/Download-Loading-Gif-Generator-Transparent-Background-PNG-.gif'/>)
+        : ('')}
+      <Form onSubmit={handleSubmit}>
+        <Form.Group controlId='formFile' className='mb-3' id='upload-file-input'>
+          <Form.Label>Upload Your Pic Yo</Form.Label>
+          <Form.Control type='file' onChange={handleChange} />
+          <Button variant='primary' type='submit' value='Submit'>
+                Submit
+          </Button>
+        </Form.Group>
+      </Form>
+    </div>
+  )
+}
