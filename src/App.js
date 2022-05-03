1dfabd55543d78a397e4eafa0a406b78ab4592a3
robinson4623@gmail.com <robinson4623@gmail.com>
2022-05-02 18:14:07 -0600

    API call to upload user image to AWS S3 bucket working, returns info including URL to file.
    - Built functionality to display uploaded image
    - Added a spinning loading gif

--- src/App.js
+++ src/App.js
@@ -19,13 +19,15 @@ const FormData = require('form-data')
 
 function App () {
   const [selected, setSelected] = useState(null)
+  const [upload, setUpload] = useState({})
+  const [loading, setLoading] = useState(false)
+
   const handleChange = (event) => {
-    console.log(event.target.files[0])
     setSelected(event.target.files[0])
   }
   const handleSubmit = (event) => {
     event.preventDefault()
-    console.log('click')
+    setLoading(true)
 
     const data = new FormData()
     data.append('upload', selected)
@@ -34,7 +36,8 @@ function App () {
       method: 'POST',
       data
     })
-      .then(console.log)
+      .then(res => setUpload(res.data.upload))
+      .then(() => setLoading(false))
       .catch(console.error)
   }
   // [selected, setSelected] = useState(null)
@@ -68,7 +71,10 @@ function App () {
   // const { msgAlerts, user } = this.state
 
   return (
-    <div>
+
+    <div className="App">
+      {upload.url ? (<img className="display-image" alt={upload.url} src={upload.url} />) : ''}
+      {loading ? (<img className="display-loading" alt="loading gif" src="https://flevix.com/wp-content/uploads/2019/07/Curve-Loading.gif"/>) : ''}
       <Form onSubmit={handleSubmit}>
         <Form.Group controlId="formFile" className="mb-3" id="upload-file-input" >
           <Form.Label>Upload Your Pic Yo</Form.Label>
