5174687f88fdfc8993e7c529b72d153fe1c43e82
raytrott <raytrott2@gmail.com>
2022-05-06 15:20:09 -0400

    Update package.json and apiConfig for production

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,6 +1,6 @@
 let apiUrl
 const apiUrls = {
-  production: 'https://aqueous-atoll-85096.herokuapp.com',
+  production: 'https://obscure-reef-87011.herokuapp.com',
   development: 'http://localhost:4741'
 }
 
